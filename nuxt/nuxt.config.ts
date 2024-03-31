a9e5fb69350771a9c90b3fec3519d6e083211f02
Thomas Venturini <thomas@venturini.codes>
2024-03-31 13:16:27 +0200

    working on strapi nuxt connection

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -1,10 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  modules: ['@nuxtjs/strapi'],
   typescript: {
     typeCheck: true,
   },
   devServer: {
     port: 3000,
+  },
+  strapi: {
+    url: 'strapi',
   },
 })
